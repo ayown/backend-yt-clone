bfb64de70f4fa12870626cba4f9992b545ebcd41
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2026-08-10 02:09:05 +0530

    remove commented-out MongoDB connection code and clean up index.js

--- src/index.js
+++ src/index.js
@@ -23,32 +23,3 @@ connectDB()  //gives a promise
   console.error("Failed to connect to the database:", error);
   process.exit(1);
 });
-
-
-
-
-
-
-
-
-
-
-/*
-;(async () => {
-  try {
-    await mongoose.connect(
-        `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
-    app.on("error", (error) => {
-      console.log("Error in server:", error);
-      throw error;
-    });
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 8000}`);
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-})();
-*/
